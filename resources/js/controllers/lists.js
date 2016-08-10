1d830e580440bdcffbf58a04949cc4b1f5f58ce1
bilkiss <bilkiss.dulloo@gmail.com>
2016-08-10 16:54:07 +0400

    Added a dropdown list for "Besteller List Names". Dropdown list with values of "display_name".

--- resources/js/controllers/lists.js
+++ resources/js/controllers/lists.js
@@ -1,34 +1,113 @@
 myApp.controller('listsController', ['$scope','$http', function($scope,$http) {
 
-    var url = "https://api.nytimes.com/svc/books/v3/lists.json";
-    url += '?api-key=362f4508e923486dbd28ee49cf4693b9&list=e-book-fiction&weeks-on-list=1';
+
+    $scope.fetchBooksInCat = function(category){
+        var url = "https://api.nytimes.com/svc/books/v3/lists.json";
+        url += '?api-key=362f4508e923486dbd28ee49cf4693b9&list='+category;
+
+        $http({
+            method: 'GET',
+            url: url
+        }).then(function successCallback(response) {
+            console.log("success");
+            //console.log(response);
+            $scope.lists = response;
+            $scope.books = [];
+            //$scope.bestsellers_name = [];
+
+            $scope.lists.data.results.map(function(data){
+                $scope.books.push({
+                    title : data.book_details[0]['title'],
+                    rank : data.rank,
+                    rank_last_week : data.rank_last_week,
+                    author : data.book_details[0]['author'],
+                    description : data.book_details[0]['description']
+                })
+            });
+
+            $scope.published_date = $scope.lists.data.results[0]['published_date'];
+        }, function errorCallback(response) {
+            console.log("error");
+            console.log(response);
+        });
+    };
+
+    $scope.DropDownChanged = function () {
+        //console.log("badaboom8888");
+        $scope.DropDownStatus = $scope.dropValue;
+        $scope.fetchBooksInCat($scope.DropDownStatus);
+    };
+
+    var bestsellersUrl = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json";
+    bestsellersUrl += '?api-key=362f4508e923486dbd28ee49cf4693b9';
+
 
     $http({
         method: 'GET',
-        url: url
+        url: bestsellersUrl
     }).then(function successCallback(response) {
-        console.log("success");
-        console.log(response);
-        $scope.lists = response;
-        $scope.books = [];
+        console.log("success on bestsellersUrl");
+        //console.log(response);
+        $scope.list_bestsellers = response;
+        $scope.bestsellers = [];
 
-        $scope.lists.data.results.map(function(data){
-            $scope.books.push({
-                title : data.book_details[0]['title'],
-                rank : data.rank,
-                rank_last_week : data.rank_last_week,
-                author : data.book_details[0]['author'],
-                description : data.book_details[0]['description'],
-            })
-        })
-        //console.log($scope.lists.data.results[0]);
-        console.log($scope.books);
+        $scope.list_bestsellers.data.results.map(function(data)
+        {
+            if(data.ranks_history.length != 0 && data.title != undefined){
+                $scope.bestsellers.push({
+                    display_name : data.ranks_history[0]['display_name'],
+                    list_name : data.ranks_history[0]['list_name'],
+                    bestseller_title : data.title
+                })
+            }
+        });
 
-        $scope.published_date = $scope.lists.data.results[0]['published_date'];
     }, function errorCallback(response) {
-        console.log("error");
+        console.log("error on bestsellersUrl");
         console.log(response);
     });
+
+
+    $scope.fetchBooksInCat('e-book-fiction');
+    //var url = "https://api.nytimes.com/svc/books/v3/lists.json";
+    //url += '?api-key=362f4508e923486dbd28ee49cf4693b9&list=e-book-fiction';
+    //
+    //$http({
+    //    method: 'GET',
+    //    url: url
+    //}).then(function successCallback(response) {
+    //    console.log("success");
+    //    //console.log(response);
+    //    $scope.lists = response;
+    //    $scope.books = [];
+    //    //$scope.bestsellers_name = [];
+    //
+    //    $scope.lists.data.results.map(function(data){
+    //        $scope.books.push({
+    //            title : data.book_details[0]['title'],
+    //            rank : data.rank,
+    //            rank_last_week : data.rank_last_week,
+    //            author : data.book_details[0]['author'],
+    //            description : data.book_details[0]['description']
+    //        })
+    //    });
+    //    //console.log($scope.lists.data.results[0]);
+    //    //console.log($scope.books);
+    //    //console.log($scope.lists);
+    //
+    //    $scope.published_date = $scope.lists.data.results[0]['published_date'];
+    //}, function errorCallback(response) {
+    //    console.log("error");
+    //    console.log(response);
+    //});
+
+
+
+
+    $scope.slugify = function(value) {
+        $scope.value = value.replace(/\s/g, "-");
+        return $scope.value;
+    };
 
     $scope.value = 'rank';
     $scope.sorting = 'rank';
@@ -36,10 +115,7 @@ myApp.controller('listsController', ['$scope','$http', function($scope,$http) {
 
     $scope.dynamicOrder = function(value) {
         //console.log($scope.order);
-        //return $scope.order;
         $scope.reverse = ($scope.value === value) ? !$scope.reverse : false;
         $scope.sorting = value;
-        console.log(value);
-
-    }
+    };
 }]);
